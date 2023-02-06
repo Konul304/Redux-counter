780aadf10c18eb69151e8226223a84dd91023985
Konul304 <memmedovakonul270@gmail.com>
2023-02-06 15:07:12 +0400

    counter

--- test/src/features/counter/Counter.js
+++ test/src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from "./counterSlice";
+import { decrement, increment, incrementByAmount } from "./counterSlice";
 
 export function Counter() {
     const count = useSelector((state) => state.counter.value)
@@ -15,13 +15,17 @@ export function Counter() {
                 >
                     Increment
                 </button>
-                <span>{count}</span>
+                <button aria-label="Increment value by amount"
+                    onClick={() => dispatch(incrementByAmount(10))}>
+                    Increment value by amount
+                </button>
                 <button
                     aria-label="Decrement value"
                     onClick={() => dispatch(decrement())}
                 >
                     Decrement
                 </button>
+                <div>{count}</div>
             </div>
         </div>
     )
