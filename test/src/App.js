3b89a9637ba0a231dcb2945cca2bfaa83ab37423
Konul304 <memmedovakonul270@gmail.com>
2023-02-06 14:18:39 +0400

    additions

--- test/src/App.js
+++ test/src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter/>
+        <Counter />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey!
         </p>
         <a
           className="App-link"
